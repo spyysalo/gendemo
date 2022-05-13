e82c9c3857bb477ddf639fa05d46f03be77607d9
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2022-05-13 12:52:40 +0300

    Update serve.py

--- serve.py
+++ serve.py
@@ -27,7 +27,22 @@ def load_tokenizer_and_model(directory):
 @app.route('/')
 def generate():
     text = request.values['text']
-    return app.generator.generate(text)
+    max_length = request.values.get('max_length', 128)
+    temperature = request.values.get('temperature', 1.0)
+
+    try:
+        temperature = min(max(float(temperature), 0), 1)
+    except:
+        temperature = 1.0
+    try:
+        max_length = min(max(max_length, 16), 512)
+    except:
+        max_length = 128
+
+    generated = app.generator.generate(text, max_length, temperature)
+    print(f'got prompt "{text}", max_length {max_length}, temperature {temperature}, generated "{generated}"', file=sys.stderr,
+          flush=True)
+    return generated
 
 
 class Generator(object):
@@ -35,7 +50,7 @@ class Generator(object):
         self.tokenizer = tokenizer
         self.model = model
 
-    def generate(self, prompt):
+    def generate(self, prompt, max_length, temperature):
         encode = lambda s: self.tokenizer.encode(s, return_tensors='pt')
         decode = lambda v: self.tokenizer.decode(v)
 
@@ -43,10 +58,10 @@ class Generator(object):
         generated = self.model.generate(
             encode(prompt),
             do_sample=True,
-            max_length=128,
+            max_length=max_length,
             top_k=5,
             top_p=0.95,
-            #temperature=args.temperature,
+            temperature=temperature,
             no_repeat_ngram_size=2,
             num_return_sequences=1,
             repetition_penalty=0.9,
