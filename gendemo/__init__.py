fc5223fde53579929c280828cdbdd0f8452f2a46
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2022-05-13 10:42:31 +0300

    Initial import

---
+++ gendemo/__init__.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask, redirect
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_pyfile('config.py') #, silent=True)
+
+    from . import view
+    app.register_blueprint(view.bp)
+
+    @app.route('/')
+    def root_redirect():
+        return redirect('gendemo')
+
+    return app
